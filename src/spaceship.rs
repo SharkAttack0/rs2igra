eb8fd279e2b6d4108139cbeaab2ff787de4d63fb
RosenPT <ros.petkov@abv.bg>
2024-06-02 16:05:42 +0300

    up

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,15 +1,9 @@
 use bevy::{prelude::*, transform::commands};
 
-use crate::movement::Velocity;
+use crate::movement::{Acceleration, MovingOjectBundle, Velocity};
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
-
-#[derive(Bundle)]
-struct spaceshipBundle{
-    velocity: Velocity,
-    model: SceneBundle
-}
 
 pub struct SpaceshipPlugin;
 
@@ -20,10 +14,11 @@ impl Plugin for SpaceshipPlugin{
 }
 
 fn spawn_spaceship(mut commands: Commands, asset_server:Res<AssetServer>) {
-    commands.spawn(spaceshipBundle {
+    commands.spawn(MovingOjectBundle {
         velocity: Velocity{
             value:STARTING_VELOCITY
         },
+        acceleration: Acceleration::new(Vec3::ZERO),
         model:SceneBundle{
             scene: asset_server.load("Spaceship.glb#Scene0"),
             transform:Transform::from_translation(STARTING_TRANSLATION),
